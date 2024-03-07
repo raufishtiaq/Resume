647fbc0f664cbcc09a9c906a5e19899cf805bcec
Muhammad Rauf <83113177+raufishtiaq@users.noreply.github.com>
2024-03-07 09:56:36 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -21,12 +21,28 @@ const fetchAndCache = async (request) => {
     try {
         const cache = await caches.open(CACHE_NAME);
         const response = await fetch(request);
-        await cache.put(request, response.clone());
-        return response;
+        
+        // Check if the response is valid
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response; // Return the original response
+        }
+
+        // Clone the response before putting it in the cache
+        const responseToCache = response.clone();
+        await cache.put(request, responseToCache);
+        
+        return response; // Return the original response
     } catch (error) {
-        return caches.match(request);
+        // If fetching fails, try to return from cache
+        const cachedResponse = await caches.match(request);
+        if (cachedResponse) {
+            return cachedResponse; // Return the cached response if available
+        }
+        
+        throw error; // Throw the error if no cached response is available
     }
 }
+
 
 self.addEventListener('install', (event) => {
     event.waitUntil(cacheAssets());
