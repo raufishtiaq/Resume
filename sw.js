a2993f6ce443ea320c66bfd61ccecc419794dead
Muhammad Rauf <83113177+raufishtiaq@users.noreply.github.com>
2024-03-07 09:43:48 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,37 @@
 console.log("This message is from the service worker");
 
+const CACHE_NAME = 'my-cache-v1';
+const CACHE_URLS = [
+    '/',
+    '/index.html', 
+    '/styles.css' 
+];
+
+const cacheAssets = async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(CACHE_URLS);
+}
+
+const fetchAndCache = async (request) => {
+    try {
+        const cache = await caches.open(CACHE_NAME);
+        const response = await fetch(request);
+        await cache.put(request, response.clone());
+        return response;
+    } catch (error) {
+        return caches.match(request);
+    }
+}
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(cacheAssets());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetchAndCache(event.request));
+});
+
+
 self.addEventListener('push', event => {
     const data = event.data.json();
     const options = {
